4a5a4c3fa07696cc91cab1dddfff1920795f5d7a
Dariush Faradjollah <d.faradjollah@gmail.de>
2013-03-17 22:52:56 +0100

    working on player and the items if calls

--- src/greyzone/creature/Player.java
+++ src/greyzone/creature/Player.java
@@ -4,6 +4,7 @@ package greyzone.creature;
 import greyzone.items.Clue;
 import greyzone.items.Food;
 import greyzone.items.Item;
+import greyzone.items.Notebook;
 import greyzone.trigger.Trigger;
 
 import java.util.ArrayList;
@@ -26,17 +27,12 @@ public class Player extends Creature implements Camera
 {
     private TermPanel term;
     private ViewField fov;
+    
     private int stepCount =0;
     private int hpDec = 10; // hp decremented every hitpointDec steps
-    private int itemsHeld;
-    private int bodyCount;
+    private int itemsHeld=0;
+    private int bodyCount=0;
     
-    
-    //private int strength;
-    //private int experience;
-    //private Coordinate; // this is where the player should be 
-    					// placed when he enters a new level.
-
 
     public Player(TermPanel term)
     {
@@ -90,32 +86,14 @@ public class Player extends Creature implements Camera
 	public void setItemsHeld(int itemsHeld) {
 		this.itemsHeld = itemsHeld;
 	}
-    /*
-    public void setStrength(int s)
-    {
-    	strength = s;
-    }
-    public void setExperience(int e)
-    {
-    	experience = e;	
-    }
-    public int getStrength()
-    {
-    	return strength;
-    }
-    public int getExperience()
-    {
-    	return experience;
-    }
-    */
+
     ////////////////////////////////////////////////////////////////
     //////////// Methods that were already implemented
     ////////////////////////////////////////////////////////////////
     @Override
     public void act()
     
-    {
-    	Actor actor;
+    {	Actor actor;
    
         try
         {
@@ -194,31 +172,75 @@ public class Player extends Creature implements Camera
 	 */
 	
 	public void interaction()
-	{
-		
-		if (getWorld().getActorsAt(greyzone.creature.Monster.class , pos()) != null)
-		{
+	{	
+		if (getWorld().getActorsAt(greyzone.creature.Monster.class , pos()) != null){
 			Collection<Monster> monsterCol = 
 					getWorld().getActorsAt(greyzone.creature.Monster.class , pos());
 			for (Monster monster : monsterCol){
 				attack(monster);
 			}
 		}
-		else if (getWorld().getActorAt(greyzone.items.Clue.class , pos()) != null)
-		{
-				Clue clue = getWorld().getActorAt(greyzone.items.Clue.class, pos());
-				handleClue(clue);
+		else if (getWorld().getActorAt(greyzone.items.Clue.class , pos()) != null) {
+			Clue clue=getWorld().getActorAt(greyzone.items.Clue.class , pos());
+			handleClue(clue);
 		}
-		// else if     the same thing for notebook
-		else if (getWorld().getActorAt(greyzone.items.Food.class , pos()) != null)
-		{
-			Food food = getWorld().getActorAt(greyzone.items.Food.class , pos());
+		
+		else if (getWorld().getActorAt(greyzone.items.Notebook.class , pos()) != null) {
+			Notebook notebook=getWorld().getActorAt(greyzone.items.Notebook.class , pos());
+			handleNotebook(notebook);
+		}
+		else if (getWorld().getActorAt(greyzone.items.Food.class , pos()) != null) {
+			Food food=getWorld().getActorAt(greyzone.items.Food.class , pos());
+			handleFood(food);
+		}
+		
+		
+		else if (getWorld().getActorAt(Trigger.class , pos()) != null ) {
+			Trigger trigger=getWorld().getActorAt(Trigger.class , pos());
+			handleTrigger(trigger);
 		}
 	}
+	
+	private void handleClue(Clue clue)
+	{
+		this.itemsHeld += 1;
+		
+		//show text: h1_item_exp.txt
+		//quit when q is pressed
+		
+		clue.expire();
+		//if (this.itemsHeld==clue.cluesHidden) 
+	}
+	
+	private void handleNotebook(Notebook notebook)
+	{
+		this.itemsHeld += 1;
+		
+		//show text: l1_item_exp.txt
+		//quit when q is pressed
+		
+		notebook.expire();
+	}
+	
+	private void handleFood(Food food)
+	{
+		this.setHp(getHp() +food.getValue());
+		
+		//show text: uEat.txt
+		//quit when q is pressed
+		
+		food.expire();
+		}
+	
+	private void handleTrigger(Trigger trigger)
+	{
+		// is itemsHeld = numOfclues ?
+		// if yes react
+	}
+	
 	
 	private void addStep()
 	{
 		setStepCount(getStepCount()+1);
 	}
-
 }
