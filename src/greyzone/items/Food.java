2ef2b52648d37ffb6f6d5b20162df8479b57a6b6
Dariush Faradjollah <d.faradjollah@gmail.de>
2013-03-20 10:45:46 +0100

    working on food class

--- src/greyzone/items/Food.java
+++ src/greyzone/items/Food.java
@@ -1,40 +1,76 @@
 package greyzone.items;
 
-import jade.util.Dice;
+import java.awt.Color;
+
+import greyzone.creature.Player;
+import jade.ui.Terminal;
 import jade.util.datatype.ColoredChar;
 
 public class Food extends Item
 {
 
-	private final int maxNutrition;
-	private final int nutritionLevel = setValue();
-	
-	public Food(ColoredChar face, String name) {
-		super(face, name);
-		maxNutrition = 5;
-	}
-	
-	public Food(ColoredChar face, String name, int maxNutrition)
-	{
-		super(face, name);
-		this.maxNutrition = maxNutrition;
+		// this should be reset whenever a new level is loaded
+		private static int nextNotebook = 0;
+		// this can be set according to the number of clues in the level loaded
+		private static int totalNumOfNotebooks = 4; // counting from 0
+		// {@code firstOrLast} is used, at the moment, to print out the first and last {@code textPaths} and {@code framePaths}
+		private static int firstOrLast = 0; 
+		// {@code messageTimer} counts the number of tick()s before a message is removed from the screen
+
+
+
+		private int messageTimer = 4;
+		private boolean startTime;
+		private int endTime;
 		
-	}
 
-	public int getValue() {
-		return nutritionLevel;
-	}
-	
-	private int setValue()
-	{
-		Dice dice = new Dice();
-		return dice.nextInt(1,maxNutrition);
-	}
-
-	@Override
-	public void act() {
-		// TODO Auto-generated method stub
+		public Food() {
+			this(ColoredChar.create('N'),"Notebook1");
+		}
+		public Food(ColoredChar face, String name) 
+		{
+			super(face, name);
+			startTime = false;
+		}
 		
-	}
+		@Override
+		public void act() 
+		{
+			if(startTime && endTime > messageTimer)
+				this.expire();
+			endTime = (endTime +1) % messageTimer;
+			
+		}
+		
+		public void handleFood() throws InterruptedException
+		{
+			printMessage();
+	    	startTime = true;
+	    	endTime = 0; // starts at 0 and ends at {@code messageTimer}
+	    	nextNotebook = (nextNotebook + 1) % totalNumOfNotebooks; // auto reset if always the same number of notebooks
+	    	getWorld().removeActor(this);
+			
+		}
+		
+		/*
+		 * @author dariush
+		 * {@code printMessage} uses the int variable {@code nextNotebook} to keep track of
+		 * the number of {@code Notebook}s found by {@code Player}. 
+		 * Only the first and last {@code Notebook}s found have file.txt to read from. These are
+		 * tracked with the int variable {@code firstOrLast}. 
+		 * this handles the output to screen for Notebook
+		 */
+		public void printMessage() throws InterruptedException
+		{
+			Terminal term = getWorld().getActor(Player.class).getTerm();
+			
+			// we want to print out a from a specific file for the first and last Notebooks found
 
+				while(term.getKey() != 'c')
+				{
+			    term.bufferString(10, 41, "Yummy !! FOOD !!", Color.cyan);
+			    term.refreshScreen();
+				}
+
+		}		
 }
