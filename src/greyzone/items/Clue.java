2e32075fd167c7bc7d1d9362e6ee73be1fac7d40
Dariush Faradjollah <d.faradjollah@gmail.de>
2013-03-21 14:03:35 +0100

    copy pasted the money class into the other items. The messages have to be retrieveMessage()ed somewhere.

--- src/greyzone/items/Clue.java
+++ src/greyzone/items/Clue.java
@@ -1,127 +1,24 @@
 package greyzone.items;
 
-import greyzone.creature.Player;
-
-import java.awt.Color;
-import jade.ui.Terminal;
+import jade.core.Actor;
 import jade.util.datatype.ColoredChar;
 
-
-public class Clue extends Item
+public class Clue extends Actor
 {
-	
-	// this should be reset whenever a new level is loaded
-	private static int nextClue = 0;	// this can be set according to the number of clues in the level loaded
-	private static int totalNumOfClues = 4; // counting from 0
-	// {@code firstOrLast} is used, at the moment, to print out the first and last {@code textPaths} and {@code framePaths}
-	private static int firstOrLast = 0; 
-	// {@code messageTimer} counts the number of tick()s before a message is removed from the screen
-	private static int messageTimer = 4;
-
-	private String[] textPaths = 	{	"screens/itemEXP/hell_item_exp.txt",
-										"screens/itemEXP/hell_allItemsFound.txt"};
-	
-	private String[] framePaths	= 	{	"screens/itemEXP/frame_item_exp.txt",
-										"screens/itemEXP/frame_allItemsFound.txt"};
-	private boolean startTime;
-	private int endTime;
-	
-	
 
 	public Clue() {
-		this(ColoredChar.create('I'),"Clue1");
-		
+		this(ColoredChar.create('Y'));
 	}
-	public Clue(ColoredChar face, String name) 
+	public Clue(ColoredChar face) 
 	{
-		super(face, name);
-		startTime = false;
-	}
-	
-	@Override
-	public void act() 
-	{
-		if(startTime && endTime == messageTimer -1)
-			this.expire();
-		Terminal term = getWorld().getActor(Player.class).getTerm();
-		endTime = (endTime +1) % messageTimer;	
-	    term.bufferString(10, 41, "You have found another Clue !!", Color.cyan);
-	    term.refreshScreen();
-	}
-	
-	// this can be called at the end of a level
-	public void resetClueCounter()
-	{
-		nextClue = 0;
+		super(face);
+		// TODO Auto-generated constructor stub
 	}
 
-	/*
-	 * @author dariush
-	 * {@code printMessage} uses the int variable {@code nextNotebook} to keep track of
-	 * the number of {@code Notebook}s found by {@code Player}. 
-	 * Only the first and last {@code Notebook}s found have file.txt to read from. These are
-	 * tracked with the int variable {@code firstOrLast}. 
-	 * this handles the output to screen for Notebook
-	 */
-	public void printMessage() throws InterruptedException
-	{
-		Terminal term = getWorld().getActor(Player.class).getTerm();
-		// TODO: 	the string messages that are printed out to the screen
-		// 			should go away by themselves after three or four moves.
+	@Override
+	public void act() {
+		// TODO Auto-generated method stub
 		
-		// we want to print out a from a specific file for the first and last Notebooks found
-		if(nextClue  < 1 || nextClue == totalNumOfClues)
-		{
-			while(term.getKey() != 'c')
-			{
-			term.bufferBoxes(getWorld(), framePaths[firstOrLast], textPaths[firstOrLast]);			
-			term.refreshScreen();
-			firstOrLast = (firstOrLast + 1) % 2;
-			}
-		}
-		else
-		{
-			while(term.getKey() != 'c')
-			{
-		    term.bufferString(10, 41, "You have found another Clue !!", Color.cyan);
-		    term.refreshScreen();
-			}
-		}
-		
-    	startTime = true;
-    	endTime = 0; // starts at 0 and ends at {@code messageTimer}	
-    	nextClue = (nextClue +1) % totalNumOfClues;
-    	
-	}
-	@Override
-	public String deliverFramePath() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	@Override
-	public String deliverTextPath() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	@Override
-	public String deliverTextForGameConsole() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	@Override
-	public boolean hasText() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-	@Override
-	public boolean hasTextPath() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-	@Override
-	public boolean hasFramePath() {
-		// TODO Auto-generated method stub
-		return false;
 	}
 
 }
--- src/greyzone/items/Food.java
+++ src/greyzone/items/Food.java
@@ -1,92 +1,22 @@
 package greyzone.items;
 
-import jade.util.Dice;
+import jade.core.Actor;
 import jade.util.datatype.ColoredChar;
 
-public class Food extends Item
-{
+public class Food extends Actor{
 
-
-	private int maxNutrition = 15;
-	private final int nutritionLevel = 10;
-	
-	public Food(ColoredChar face, String name) {
-		super(face, name);
-
-	}
 	public Food() {
-		this(ColoredChar.create('F'),"Food");
+		this(ColoredChar.create('F'));
 	}
-	
-	public Food(ColoredChar face, String name, int maxNutrition)
-	{
-		super(face, name);
-		this.maxNutrition = maxNutrition;
+	public Food(ColoredChar face) {
+		super(face);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void act() {
+		// TODO Auto-generated method stub
 		
 	}
 
-	public int getValue() {
-		return nutritionLevel;
-	}
-	
-	private int setValue()
-	{
-		Dice dice = new Dice();
-		return dice.nextInt(1,maxNutrition);
-	}
-
-		private static Dice dice;
-		private int addedHitPoints = dice.nextInt(5, 15);
-
-		
-		@Override
-		public void act() 
-		{
-			
-			
-		}
-		
-		@Override
-		public String deliverFramePath() {
-			// TODO Auto-generated method stub
-			return null;
-		}
-		@Override
-		public String deliverTextPath() {
-			// TODO Auto-generated method stub
-			return null;
-		}
-		
-		
-		@Override
-		public String deliverTextForGameConsole() 
-		{		
-			return "Yummy !! Food !!" + " You got " + addedHitPoints + " Hit Points !";
-		}
-		@Override
-		public boolean hasText() 
-		{
-			return true;
-		}
-		
-		
-		@Override
-		public boolean hasTextPath() {
-			// TODO Auto-generated method stub
-			return false;
-		}
-		@Override
-		public boolean hasFramePath() {
-			// TODO Auto-generated method stub
-			return false;
-		}		
-		
-		
-		// method to be called from player to get hitpoints
-		public int getHitPointsToPlayer()
-		{
-			return addedHitPoints;
-		}
-		
-		
 }
